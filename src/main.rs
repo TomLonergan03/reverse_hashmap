8d8d1165acbe76743607ef83261f2e92d734d313
Tom Lonergan <tomlonergan91@gmail.com>
2023-02-22 18:10:01 +0000

    Reverse lookup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod reverse_hashmap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut person_and_age: reverse_hashmap::ReverseHashMap<String, u16> =
+        reverse_hashmap::ReverseHashMap::new();
+
+    person_and_age.insert("John".to_string(), 20);
+    person_and_age.insert("Jane".to_string(), 54);
+    person_and_age.insert("Joe".to_string(), 75);
+
+    let a_key: u64 = rand::random();
+    let value: Option<&u16> = person_and_age.get(a_key);
+    if value.is_some() {
+        println!("The value at key {} is {}", a_key, value.unwrap());
+    } else {
+        println!("There is no value at key {}", a_key);
+    }
+    let another_key: u64 = rand::random();
+    let delete_result: Option<u16> = person_and_age.delete(another_key);
+    if delete_result.is_some() {
+        println!("Deleted value at key {}", another_key);
+    } else {
+        println!("There was no value at key {}", another_key);
+    }
+    let john: Option<u64> = person_and_age.find_key(20, true);
+    if john.is_some() {
+        println!("John is {} years old", john.unwrap());
+    } else {
+        println!("John is not in the hashmap");
+    }
+    return;
 }
--- src/reverse_hashmap.rs
+++ src/reverse_hashmap.rs
@@ -1,26 +1,72 @@
-pub struct ReverseHashMap<K, V> {
-    map: HashMap<K, V>,
+use std::collections::hash_map::DefaultHasher;
+use std::collections::{hash_map, HashMap};
+use std::hash::{Hash, Hasher};
+use std::marker::PhantomData;
+
+pub struct ReverseHashMap<K: Eq + Hash, V> {
+    map: HashMap<u64, V>,
     private_key: u64,
+    key_type: PhantomData<K>,
 }
 
-impl ReverseHashMap {
-    pub fn new() -> ReverseHashMap {
+#[derive(Hash, PartialEq)]
+struct KeyToHash<K: Eq + Hash> {
+    private_key: u64,
+    map_key: K,
+}
+
+impl<K: Eq + Hash, V: PartialEq> ReverseHashMap<K, V> {
+    pub fn new() -> ReverseHashMap<K, V> {
         ReverseHashMap {
             map: HashMap::new(),
-            private_key: rand::thread_rng().gen(),
+            private_key: rand::random(),
+            key_type: PhantomData,
         }
     }
 
-    pub fn insert(&mut self, key: K, value: V) {
-        let hashed_key = calculate_hash(&key, &mut self.private_key);
-        self.map.insert(hashed_key, value)
+    // inserts a value into the hashmap and returns the the previous value at that key if it exists.
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        let key_to_be_hashed: KeyToHash<K> = KeyToHash {
+            private_key: self.private_key,
+            map_key: key,
+        };
+        let mut hasher: DefaultHasher = hash_map::DefaultHasher::new();
+        key_to_be_hashed.hash(&mut hasher);
+        let hashed_key: u64 = hasher.finish();
+        return self.map.insert(hashed_key, value);
     }
 
-    pub fn get(&self, key: K) -> Option<&V> {
-        self.map.get(&key)
+    // Searches all possible values for hash and returns the first one that matches.
+    pub fn find_key(&self, value: V, print_progress: bool) -> Option<u64> {
+        for possible_key in 0..u64::MAX {
+            let key_to_be_hashed: KeyToHash<u64> = KeyToHash {
+                private_key: self.private_key,
+                map_key: possible_key,
+            };
+            let mut hasher: DefaultHasher = hash_map::DefaultHasher::new();
+            key_to_be_hashed.hash(&mut hasher);
+            let hashed_key: u64 = hasher.finish();
+            if self.map.get(&hashed_key) == Some(&value) {
+                return Some(possible_key);
+            }
+            if print_progress && possible_key % 1000000 == 0 {
+                println!(
+                    "Progress: {} tested, {}% done",
+                    possible_key,
+                    possible_key / u64::MAX * 100
+                );
+            }
+        }
+        return None;
     }
 
-    pub fn delete(&mut self, key: K) {
-        self.map.remove(&key)
+    // Returns the value at the key if it exists.
+    pub fn get(&self, key: u64) -> Option<&V> {
+        return self.map.get(&key);
+    }
+
+    // Deletes the value at the key if it exists.
+    pub fn delete(&mut self, key: u64) -> Option<V> {
+        return self.map.remove(&key);
     }
 }
